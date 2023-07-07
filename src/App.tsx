063b7b3b731154bf7b086bb607a8b74809861aa2
unknown <xlongclaw@gmail.com>
2023-07-07 23:14:28 +0530

    added navbar component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div>
-      Hello
+      <NavBar/>
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{tsx,jsx,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
