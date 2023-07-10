e6456202a249886a063bd66b265a96f07580e280
unknown <xlongclaw@gmail.com>
2023-07-10 10:50:04 +0530

    Created Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import HomePage from './Screens/HomePage'
 import RestaurantsPage from './Screens/RestaurantsPage'
+import LoginPage from './Screens/LoginPage'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<HomePage/>}/>
         <Route path='/restaurants' element={<RestaurantsPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
       </Routes>
       </BrowserRouter>
     </div>
